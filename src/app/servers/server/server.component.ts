c4d65066ad82dad3f6cfb02efd358da261bddace
Zwaks <zwakelemgabhi@gmail.com>
2023-01-13 06:04:46 +0200

    angular routing

--- src/app/servers/server/server.component.ts
+++ src/app/servers/server/server.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params, Router } from '@angular/router';
 
 import { ServersService } from '../servers.service';
 
@@ -10,10 +11,22 @@ import { ServersService } from '../servers.service';
 export class ServerComponent implements OnInit {
   server: {id: number, name: string, status: string};
 
-  constructor(private serversService: ServersService) { }
+  constructor(private serversService: ServersService,
+              private route: ActivatedRoute,
+              private router: Router) { }
 
   ngOnInit() {
-    this.server = this.serversService.getServer(1);
+    // the "+" converts the string '1' to a number 1
+    this.server = this.serversService.getServer(+this.route.snapshot.params['id']);
+    this.route.params.subscribe(
+      (params: Params) => {
+        this.server = this.serversService.getServer(+params['id']);
+      } 
+    );
+  }
+
+  onEdit() {
+    this.router.navigate(['edit'], {relativeTo: this.route, queryParamsHandling: 'preserve'});
   }
 
 }
